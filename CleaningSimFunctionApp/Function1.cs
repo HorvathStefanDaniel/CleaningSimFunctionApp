f5c5ab8e859ecde8206388258c95ae4810023a37
HSD112 <horvathstefandaniel@gmail.com>
2021-05-27 20:00:23 +0200

    FinalCommit

--- CleaningSimFunctionApp/Function1.cs
+++ CleaningSimFunctionApp/Function1.cs
@@ -8,16 +8,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Data.SqlClient;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-
-using System.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
 
 namespace CleaningSimFunctionApp
 {
